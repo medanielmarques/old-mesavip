697301d7b002fa321173a7330051113fb69d14ae
Daniel Marques <daniel.brz2009@gmail.com>
2021-12-27 23:22:06 -0300

    tabs

--- src/pages/reservations.tsx
+++ src/pages/reservations.tsx
@@ -1,5 +1,14 @@
 import { createContext, useEffect, useState } from 'react';
-import { Box, Grid, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Grid,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from '@chakra-ui/react';
 
 import { ReservationCard } from 'src/components/ReservationCard';
 import { Reservation } from 'src/interfaces/reservation';
@@ -53,35 +62,74 @@ export default function Reservations() {
     <ReservationContext.Provider
       value={{ refreshPastReservations, refreshFollowingReservations }}
     >
-      <Box
-        m='0 auto'
-        w={{
-          base: '384px',
-          md: '792px',
-          lg: '792px',
-          xl: '792px',
-          '2xl': '1300px',
-        }}
-        display='table'
-      >
-        <Text fontSize='20px' m='30px 0' align='center'>
-          Following Reservations
-        </Text>
-        <Grid templateColumns='repeat(auto-fill, minmax(384px, 1fr))' gap={6}>
-          {followingReservations.map((reservation) => (
-            <ReservationCard key={reservation.id} reservation={reservation} />
-          ))}
-        </Grid>
+      <Tabs isFitted>
+        <TabList
+          width={{
+            base: '288px',
+            md: '594px',
+            lg: '594px',
+            xl: '594px',
+            '2xl': '975px',
+          }}
+          m='30px auto'
+        >
+          <Tab>Following Reservations</Tab>
+          <Tab>Past Reservations</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Box
+              m='0 auto'
+              w={{
+                base: '384px',
+                md: '792px',
+                lg: '792px',
+                xl: '792px',
+                '2xl': '1300px',
+              }}
+              display='table'
+            >
+              <Grid
+                templateColumns='repeat(auto-fill, minmax(384px, 1fr))'
+                gap={6}
+              >
+                {followingReservations.map((reservation) => (
+                  <ReservationCard
+                    key={reservation.id}
+                    reservation={reservation}
+                  />
+                ))}
+              </Grid>
+            </Box>
+          </TabPanel>
 
-        <Text fontSize='20px' m='30px 0' align='center'>
-          Past Reservations
-        </Text>
-        <Grid templateColumns='repeat(auto-fill, minmax(384px, 1fr))' gap={6}>
-          {pastReservations.map((reservation) => (
-            <ReservationCard key={reservation.id} reservation={reservation} />
-          ))}
-        </Grid>
-      </Box>
+          <TabPanel>
+            <Box
+              m='0 auto'
+              w={{
+                base: '384px',
+                md: '792px',
+                lg: '792px',
+                xl: '792px',
+                '2xl': '1300px',
+              }}
+              display='table'
+            >
+              <Grid
+                templateColumns='repeat(auto-fill, minmax(384px, 1fr))'
+                gap={6}
+              >
+                {pastReservations.map((reservation) => (
+                  <ReservationCard
+                    key={reservation.id}
+                    reservation={reservation}
+                  />
+                ))}
+              </Grid>
+            </Box>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </ReservationContext.Provider>
   );
 }
