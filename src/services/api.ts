8714f7c783d932afd53c4c0bc3fd0b1cdeb1f431
Daniel Marques <daniel.brz2009@gmail.com>
2022-02-21 18:19:06 -0300

    .env

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,8 +3,10 @@ import { getToken } from './auth';
 
 const token = getToken();
 
+console.log(process.env.REACT_APP_API_URL);
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: process.env.NEXT_PUBLIC_REACT_APP_API_URL,
   headers: {
     Authorization: `Bearer ${token}`,
   },
