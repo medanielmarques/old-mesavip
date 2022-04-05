c58238c7d7d9b67f8cc087631e21c8332ea319e8
Daniel Marques <daniel.brz2009@gmail.com>
2022-04-04 23:54:42 -0300

    - Fixed bugged Footer - Adjusted the Restaurant Card Skeleton

--- src/components/RestaurantsList/Wrapper.tsx
+++ src/components/RestaurantsList/Wrapper.tsx
@@ -3,16 +3,17 @@ import { Footer } from 'components/Footer';
 
 export function Wrapper({ children }: BoxProps) {
   return (
-    <Box bg={{ base: 'inherit', md: 'gray.50' }} minHeight='100vh'>
-      <Box px={{ base: '6', md: '24' }} pt='8'>
-        <Stack spacing={6}>
-          <Flex gridGap='12' mx='auto'>
-            {children}
-          </Flex>
-        </Stack>
+    <>
+      <Box bg={{ base: 'inherit', md: 'gray.50' }} minHeight='100vh'>
+        <Box px={{ base: '6', md: '24' }} pt='8'>
+          <Stack spacing={6}>
+            <Flex gridGap='12' mx='auto'>
+              {children}
+            </Flex>
+          </Stack>
+        </Box>
       </Box>
-
       <Footer />
-    </Box>
+    </>
   );
 }
